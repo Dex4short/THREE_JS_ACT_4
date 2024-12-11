d2395278d10b40a04f3104e4bb64ac5ececf3bac
Dexter Pacaña <dextepacanya@gmail.com>
2024-12-11 20:48:58 +0800

    Update water_fragment.glsl

--- shaders/water_fragment.glsl
+++ shaders/water_fragment.glsl
@@ -1,6 +1,3 @@
 void main() {
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectedPosition = projectionMatrix * viewPosition;
-  gl_Position = projectedPosition;
+  gl_FragColor = vec4(0.5, 0.8, 1.0, 1.0);
 }
